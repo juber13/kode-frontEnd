c2072715b8947c06f15cab60b901ef7f96533a4d
juber <juberk1010@gmail.com>
2024-09-20 12:11:34 +0530

    added name

--- src/Header.jsx
+++ src/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
     <div className='flex justify-between items-center p-4 w-full bg-gray-100'> 
         <div className='bg-gray-100'>kode</div>
         {isLoggedIn ? (
-        <div className='right-info border p-1 font-bold' onClick={handleLogout}>Logout</div>
+        <div className='right-info border p-1 font-bold cursor-pointer' onClick={handleLogout}>Logout</div>
         ) : (
         <div className='right-info cursor-pointer border p-1 font-bold' onClick={() => navigate("/login")}>Login</div>
         )}
