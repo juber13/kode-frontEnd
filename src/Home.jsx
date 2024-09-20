db7b14a98ad362e6b18c6192b23ce77125cd8501
juber <juberk1010@gmail.com>
2024-09-20 12:09:26 +0530

    added name

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Home = () => {
     const [userName , setUserName] = useState(JSON.parse(localStorage.getItem('userInfo')));
   return (
-    <div className='w-full h-screen bg-red-300 flex items-center justify-center text-3xl'>Welcome {userName?.email.slice(0 , 5)}</div>
+    <div className='w-full h-screen flex items-center justify-center text-3xl'>Welcome {userName?.email.slice(0 , 5)}</div>
   )
 }
 
